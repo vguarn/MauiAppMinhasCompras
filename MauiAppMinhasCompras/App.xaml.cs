41f8a65ad6bfbcaacb77d4cb378005415d704392
vitória <vitoriaguarn@gmail.com>
2024-11-29 16:09:17 -0300

    alterações no projeto

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -1,12 +1,34 @@
-﻿namespace MauiAppMinhasCompras
+﻿using MauiAppMinhasCompras.Helpers;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace MauiAppMinhasCompras
 {
     public partial class App : Application
     {
+        static SQliteDatabaseHelper _db;
+        public static SQliteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(
+                            Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_compras.db3");
+
+                    _db = new SQliteDatabaseHelper(path);
+                }
+
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            //MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.ListaProduto());
         }
     }
 }
